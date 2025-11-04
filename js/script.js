4dd9b7a0f8285f0b1e583dbf762b719cd79133de
Niakate <nktmakan7@gmail.com>
2025-11-04 11:19:13 +0100

    fini

---
+++ js/script.js
@@ -0,0 +1,164 @@
+let un = document.getElementById("un");
+let deux = document.getElementById("deux");
+let trois = document.getElementById("trois");
+let quatre = document.getElementById("quatre");
+let cinq = document.getElementById("cinq");
+let six = document.getElementById("six");
+let sept = document.getElementById("sept");
+let huit = document.getElementById("huit");
+let neuf = document.getElementById("neuf");
+let zero = document.getElementById("zero");
+let efface = document.getElementById("efface");
+let pourcent = document.getElementById("pourcent");
+let diviser = document.getElementById("diviser");
+let multiplier = document.getElementById("multiplier");
+let moins = document.getElementById("moins");
+let plus = document.getElementById("plus");
+let egale = document.getElementById("egale");
+let afficheNombre = document.getElementById("afficheNombre");
+let afficheSigne = document.getElementById("afficheSigne");
+let autreNombre = document.getElementById("autreNombre");
+let resultat = document.getElementById("resultat");
+let remiseaZero = document.getElementById("raz");
+let virgule = document.getElementById("virgule");
+
+let affichage = afficheNombre;
+
+// Aficcher les nombres 
+un.addEventListener("click", function () {
+    affichage.innerText += 1;
+
+});
+
+deux.addEventListener("click", function () {
+    affichage.innerText += 2;
+
+});
+
+trois.addEventListener("click", function () {
+    affichage.innerText += 3;
+
+});
+
+quatre.addEventListener("click", function () {
+    affichage.innerText += 4;
+
+});
+
+cinq.addEventListener("click", function () {
+    affichage.innerText += 5;
+
+});
+
+six.addEventListener("click", function () {
+    affichage.innerText += 6;
+
+});
+
+sept.addEventListener("click", function () {
+    affichage.innerText += 7;
+
+});
+
+huit.addEventListener("click", function () {
+    affichage.innerText += 8;
+
+});
+
+neuf.addEventListener("click", function () {
+    affichage.innerText += 9;
+
+});
+
+zero.addEventListener("click", function () {
+    affichage.innerText += 0;
+
+});
+
+
+// Afficher les signes
+plus.addEventListener("click", function () {
+    afficheSigne.innerText = "+";
+    affichage = autreNombre
+});
+
+moins.addEventListener("click", function () {
+    afficheSigne.innerText = "-";
+    affichage = autreNombre
+});
+
+diviser.addEventListener("click", function () {
+    afficheSigne.innerText = "/";
+    affichage = autreNombre
+});
+
+multiplier.addEventListener("click", function () {
+    afficheSigne.innerText = "x";
+    affichage = autreNombre
+});
+
+virgule.addEventListener("click", function () {
+    affichage.innerText += ".";
+    
+});
+
+
+
+// Supprime tout 
+remiseaZero.addEventListener("click", function () {
+
+    afficheNombre.innerText = "";
+    afficheSigne.innerText = "";
+    autreNombre.innerText = "";
+    resultat.innerText = "";
+    affichage = afficheNombre;
+});
+
+
+efface.addEventListener("click", function () {
+
+    afficheNombre.innerText = "";
+    afficheSigne.innerText = "";
+    autreNombre.innerText = "";
+    resultat.innerText = "";
+    affichage = afficheNombre;
+});
+
+
+
+//Les oparations 
+
+egale.addEventListener("click", function () {
+    let operation = afficheSigne.innerText
+
+    switch (operation) {
+        case "+":
+            resultat.innerText = Number(afficheNombre.innerText) + Number(autreNombre.innerText);
+            break;
+
+        case "-":
+            resultat.innerText = Number(afficheNombre.innerText) - Number(autreNombre.innerText);
+            break;
+
+        case "/":
+            resultat.innerText = Number(afficheNombre.innerText) / Number(autreNombre.innerText);
+            break;
+
+
+        case "x":
+            resultat.innerText = Number(afficheNombre.innerText) * Number(autreNombre.innerText);
+            break;
+
+
+    }
+
+
+});
+
+
+
+
+
+
+
+
